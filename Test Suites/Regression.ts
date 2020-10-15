5fcbf254ca0664bd7ee5ba2c1122b402075a56e6
ganesan <ganesan@localhost>
2020-10-15 23:24:36 +0530

    commit

---
+++ Test Suites/Regression.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4580fd9f-304d-4af3-814c-fdec6c1c6403</testSuiteGuid>
+   <testCaseLink>
+      <guid>3a3e53de-98f4-4618-9832-bd9fcb8f29e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test for Git Push</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6eb0a87f-3551-4e61-ab61-925ec86557fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test fot Git Pull</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
